2562198c6951c7510475c8b88e1d010a13118a71
natalia <mellomartins.naty@gmail.com>
2021-08-29 05:46:03 -0300

    requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,23 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('2 - Verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const mockedUser = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+
+  api.fetchURL = jest.fn().mockResolvedValue(mockedUser);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
@@ -37,3 +53,5 @@ describe('2 - Verifica o usuário', () => {
     })
   ));
 });
+
+// referência para correção da função: https://github.com/tryber/sd-014-b-project-jest/pull/72/files
