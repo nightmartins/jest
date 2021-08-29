6f527888058349f83748eb45b81e13f70a032469
natalia <mellomartins.naty@gmail.com>
2021-08-29 02:26:45 -0300

    teste requisito 1 para ver se o avaliador roda

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,23 +1,25 @@
-// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
-// const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
-
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
-
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('1 - O retorno do telefonema', () => {
-  test('atende', () => {
-    expect(answerPhone(true)).resolves.toEqual('Oi!');
+  test('atende', async () => {
+    // assert.fail();
+    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    await expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
-  test('ocupado', () => {
-    expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
+
+  test('ocupado', async () => {
+    // assert.fail();
+    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    await expect(answerPhone(false)).rejects
+      .toEqual(new Error('Infelizmente não podemos atender...'));
   });
 });
-
-// Referência: https://github.com/tryber/sd-014-b-project-jest/pull/20/files
